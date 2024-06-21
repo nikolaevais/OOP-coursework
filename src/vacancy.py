7338646fb321b6d6f93eefb771b37886502973c6
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-06-22 01:44:59 +0300

    write __repr__ and staticmethod

--- src/vacancy.py
+++ src/vacancy.py
@@ -8,4 +8,31 @@ class Vacancy:
         self.city = city
         self.employment_name = employment
 
+    def __repr__(self):
+        return (f'Название вакансии: {self.name};  Ссылка: {self.url};  Зарплата от: {self.salary_from};  Город: {self.city};   Тип занятости: {self.employment_name}')
+
+    @staticmethod
+    def validate_salary(salary):
+        if salary:
+            salary_from = salary.get('from')
+            if salary_from:
+                return salary_from
+        return 0
+
+    @staticmethod
+    def validate_city(area):
+        if area:
+            city = area.get('name')
+            if city:
+                return city
+        return "Город не указан"
+
+
+    @staticmethod
+    def validate_employment(employment):
+        if employment:
+            employment_name = employment.get('name')
+            if employment_name:
+                return employment_name
+        return "не указан"
 
