0338325f91a65480739c675fb917dcdd609b9dc5
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-06-22 01:28:14 +0300

    write a class to connect the API to HH.ru

--- src/api_hh.py
+++ src/api_hh.py
@@ -3,3 +3,11 @@ class BaseApi(ABC):
     @abstractmethod
     def get_vacancy(self, keyword):
         pass
+
+
+class HHApi(BaseApi):
+    "класс для подключения к API HH.ru"
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
