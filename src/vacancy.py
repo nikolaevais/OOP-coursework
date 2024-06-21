f88679c09294ca72378215bb3ea9a0cb9be6c40f
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-06-22 01:42:49 +0300

    write a class to work with job vacancy data

---
+++ src/vacancy.py
@@ -0,0 +1,11 @@
+
+class Vacancy:
+    "класс для работы с данными вакансий"
+    def __init__(self, name, url, salary_from, city, employment):
+        self.name = name
+        self.url = url
+        self.salary_from = salary_from
+        self.city = city
+        self.employment_name = employment
+
+
