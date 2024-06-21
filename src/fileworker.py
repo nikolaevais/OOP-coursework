32f4514ab8e385fc6d37139f1916df832117ec62
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-06-22 01:39:58 +0300

    write a class to work with json files, write a function to save and open a file

--- src/fileworker.py
+++ src/fileworker.py
@@ -9,3 +9,26 @@ class SaveFile(ABC):
     def read_file(self):
         pass
 
+
+class JSONWorker(SaveFile):
+    """
+    класс для работы с json файлами
+    """
+    def __init__(self, file_name: str):
+        self.path = os.path.join(ROOT_DIR, 'data', file_name)
+
+    def read_file(self, file):
+        "функция для открытия файла"
+        with open(self.path, encoding="UTF-8") as file:
+            data = json.load(file)
+        vacancies = []
+        for vacanci in data:
+            vacancies.append(Vacancy(**vacanci))
+        return vacancies
+
+
+
+    def write_file(self, data):
+        "функция для сохранения файла"
+        with open(self.path, 'w', encoding="UTF-8") as file:
+            return json.dump(data, file, indent=4, ensure_ascii=False)
