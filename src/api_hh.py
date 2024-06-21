01df396573f09518dabeada4c6ab7b2dec56821d
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-06-22 01:30:12 +0300

    write a function to search for vacancies on the HH.ru website using a keyword

--- src/api_hh.py
+++ src/api_hh.py
@@ -11,3 +11,10 @@ class HHApi(BaseApi):
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'text': '', 'page': 0, 'per_page': 100}
+
+
+    def get_vacancy(self, keyword):
+        "поиск вакансий на сайте HH.ru по ключевому слову"
+        self.params.update({'text': keyword})
+        response = requests.get(self.url,params=self.params)
+        return response.json()['items']
